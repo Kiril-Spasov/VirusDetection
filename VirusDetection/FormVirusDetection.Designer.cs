05aa2943de54678a8023121c9249cba2dbf841a8
Kiril Spasov <spasov.kiril@gmail.com>
2023-01-11 17:07:24 +0200

    Add project files.

---
+++ VirusDetection/FormVirusDetection.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace VirusDetection
+{
+    partial class FormVirusDetection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCheck = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnCheck
+            // 
+            this.BtnCheck.Location = new System.Drawing.Point(27, 55);
+            this.BtnCheck.Name = "BtnCheck";
+            this.BtnCheck.Size = new System.Drawing.Size(171, 55);
+            this.BtnCheck.TabIndex = 0;
+            this.BtnCheck.Text = "Check for Viruses";
+            this.BtnCheck.UseVisualStyleBackColor = true;
+            this.BtnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(229, 55);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(455, 348);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormVirusDetection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnCheck);
+            this.Name = "FormVirusDetection";
+            this.Text = "Virus Detection";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCheck;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ VirusDetection/FormVirusDetection.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VirusDetection
+{
+    public partial class FormVirusDetection : Form
+    {
+        public FormVirusDetection()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            string line = "";
+
+            string path = Application.StartupPath + @"\virus.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            bool finished = false;
+
+            while (!finished)
+            {
+                line = streamReader.ReadLine();
+
+                if (line == null)
+                {
+                    finished = true;
+                }
+                else
+                {
+                    if (CheckForVirus(line))
+                    {
+                        TxtResult.Text += line + " has occurance of the string - virus" + Environment.NewLine;
+                    }
+                }
+            }
+        }
+
+        private bool CheckForVirus(string input)
+        {
+            string character;
+            bool hasVirus = true;
+            string word = "virus";
+            int index = 0;
+            int count = 0;
+
+            string toLower = input.ToLower();
+
+            while (count < word.Length)
+            {
+                index = toLower.IndexOf(word[count]);
+
+                if (index == -1)
+                {
+                    hasVirus = false;
+                    break;
+                }
+                else
+                {
+                    toLower = toLower.Substring(index + 1);
+                    count++;
+                }
+            }
+            return hasVirus;
+        }
+    }
+}
